b74d44b15b22c805709de521c7f3559ca5f94541
TheAbhilash23 <theak.92@gmail.com>
2024-01-01 14:37:11 +0530

    added cache implementation for list viewset of restframework by overriding list method

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,5 +1,7 @@
 from rest_framework import permissions, viewsets
+from rest_framework.response import Response
 
+from caching import invalidatable_cache
 from profiles import serializers
 from profiles import models
 
@@ -9,4 +11,20 @@ class ProfileViewSet(viewsets.ModelViewSet):
     queryset = models.Profile.objects.all()
     serializer_class = serializers.ProfileSerializer
     # permission_classes = [permissions.IsAuthenticated]
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        # page = self.paginate_queryset(queryset)
+        # if page is not None:
+        #     serializer = self.get_serializer(page, many=True)
+        #     return self.get_paginated_response(serializer.data)
+
+        @invalidatable_cache('user')
+        def serialization():
+            this_data = self.get_serializer(queryset, many=True)
+            return this_data.data
+        data = serialization()
+
+        return Response(data)
 
