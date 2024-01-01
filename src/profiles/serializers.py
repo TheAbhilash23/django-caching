cf0b183815655c240c6d7f4443baea71a13c624f
TheAbhilash23 <theak.92@gmail.com>
2024-01-01 14:36:12 +0530

    added a sleep timer in Base64Picture serializer methodfield to check cache performance

--- src/profiles/serializers.py
+++ src/profiles/serializers.py
@@ -1,13 +1,12 @@
 from rest_framework import serializers
 from profiles import models
 import base64
-from caching import cache_this
+# from caching import cache_this_method?
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     Base64Picture = serializers.SerializerMethodField()
 
-    @cache_this
     def get_Base64Picture(self, obj):
         import time
         time.sleep(5)
